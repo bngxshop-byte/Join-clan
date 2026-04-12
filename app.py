8d3bf999d2a18d7806cc0fe05185c86386f41bcc
bngxshop-byte <bngxshop@gmail.com>
2026-04-12 21:33:49 +0100

    Update app.py

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ import reqClan_pb2
 import jwt as pyjwt
 
 # ====== الإعدادات الأساسية ======
-freefire_version = "OB52"
+freefire_version = "OB53"
 key = bytes([89, 103, 38, 116, 99, 37, 68, 69, 117, 104, 54, 37, 90, 99, 94, 56])
 iv = bytes([54, 111, 121, 90, 68, 114, 50, 50, 69, 51, 121, 99, 104, 106, 77, 37])
 JWT_REGEX = re.compile(r'(eyJ[A-Za-z0-9_\-\.=]+)')
